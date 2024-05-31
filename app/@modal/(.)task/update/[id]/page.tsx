0b08b05396daa31f101a33b0b4f0fcef6fb85be8
gianevag <gianevag@hotmail.com>
2024-05-31 11:19:18 +0300

    fix[crud-task]: fix warning

--- app/@modal/(.)task/update/[id]/page.tsx
+++ app/@modal/(.)task/update/[id]/page.tsx
@@ -25,6 +25,7 @@ export default function Page({ params }: { params: { id: string } }) {
       }
     }
     fetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
